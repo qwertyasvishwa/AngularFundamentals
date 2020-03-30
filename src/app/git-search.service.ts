a39663e615cb9a629570d6f5d788c5a4c1cd07b5
Vishwa Raj <qwertyasvishwa@outlook.com>
2020-03-30 20:19:15 +0530

    Added Promise and Observables

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitSearchService {
+  constructor(private http: HttpClient) { 
 
-  constructor() { }
+  }
+  sampleObservable = () => {
+    return this.http.get('https://api.github.com/search/repositories?q=');
+  }
+  samplePromise = () => {
+    return this.http.get('https://api.github.com/search/repositories?q=').toPromise();
+  }
+
+  usingObservable = () => {
+    let apiCall = this.sampleObservable();
+    apiCall.subscribe((result) => {
+      console.log(result);
+    }, (error) => {
+      console.log(error);
+    })
+    apiCall.subscribe();
+  }
+  usingPromise = () => {
+    this.samplePromise().then((result) => {
+      console.log(result);
+    }, (error) => {
+      console.log(error);
+    })
+  }
 }
