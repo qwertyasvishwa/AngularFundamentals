ce78d1fee82bc6bb3e262417083caab388da54f2
Vishwa Raj <qwertyasvishwa@outlook.com>
2020-04-02 13:28:22 +0530

    Consume the new method in the application

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -1,34 +1,32 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { Injectable, Inject } from '@angular/core';
+import { GitSearch } from './git-search';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class GitSearchService {
-  constructor(private http: HttpClient) { 
-
-  }
-  sampleObservable = () => {
-    return this.http.get('https://api.github.com/search/repositories?q=');
-  }
-  samplePromise = () => {
-    return this.http.get('https://api.github.com/search/repositories?q=').toPromise();
+  cachedValues: Array<{
+    [query: string]: GitSearch
+  }> = [];
+  constructor(private http: HttpClient) {
+      
   }
 
-  usingObservable = () => {
-    let apiCall = this.sampleObservable();
-    apiCall.subscribe((result) => {
-      console.log(result);
-    }, (error) => {
-      console.log(error);
+  gitSearch = (query: string): Promise<GitSearch> => {
+    let promise = new Promise<GitSearch>((resolve, reject) => {
+        if (this.cachedValues[query]) {
+            resolve(this.cachedValues[query])
+        }
+        else {
+            this.http.get('https://api.github.com/search/repositories?q=' + query)
+            .toPromise()
+            .then( (response) => {
+                resolve(response as GitSearch)
+            }, (error) => {
+                reject(error);
+            })
+        }
     })
-    apiCall.subscribe();
-  }
-  usingPromise = () => {
-    this.samplePromise().then((result) => {
-      console.log(result);
-    }, (error) => {
-      console.log(error);
-    })
+    return promise;
   }
 }
