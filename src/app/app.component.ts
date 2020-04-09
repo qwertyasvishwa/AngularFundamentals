ef4f5de7bd9235d1686011fd45499c640e0902be
Vishwa Raj <qwertyasvishwa@outlook.com>
2020-04-09 18:23:56 +0530

    Added the components git-search. and modified app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,13 @@ import { GitSearchService } from './git-search.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GitSearchService]
 })
 export class AppComponent implements OnInit {
-  constructor(private GitSearchService: GitSearchService) {
-  }
+  
   ngOnInit() {
-    this.GitSearchService.gitSearch('html').then((response) => {
-      alert("Total Libraries Found:" + response.items['id']);
-    }, (error) => {
-      alert("Error: " + error.statusText)
-    })
+  
   }
   title = 'GitHub Browser';
 }
